19753f220c7c217d01db92579fd88d2361520082
Anurag Bansal <anurag2003bansal@gmail.com>
2024-08-25 11:58:35 +0530

    Modifications

--- qualifier/settings.py
+++ qualifier/settings.py
@@ -76,7 +76,10 @@ WSGI_APPLICATION = 'qualifier.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
